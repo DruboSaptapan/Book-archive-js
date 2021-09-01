17156f94cce9ddc8dbe5d59422279e749b3adc38
DruboSaptapan <saptapan2017@gmail.com>
2021-09-02 00:22:25 +0600

    load data

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-console.log('js file added')
+const searchingBook = () =>{
+    const searchingfield = document.getElementById('search-field');
+    const searchBook = searchingfield.value;
+    
+    // clear searching field
+    searchingfield.value = '';
+
+    // load data of books
+    const url = `https://openlibrary.org/search.json?q=${searchBook}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => console.log(data))
+}
+
+// searched book in the display
